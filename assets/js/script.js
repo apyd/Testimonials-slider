c4bfe79beff471137491cada5554303720d083f1
apyd <arturpydych@gmail.com>
2020-10-04 16:09:53 +0200

    Script improvements
    - Keeping activeSliderId in variable
    - Extendable (works also with more slides)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,48 @@
 const prevButtons = document.querySelectorAll('.btn--left');
 const nextButtons = document.querySelectorAll('.btn--right');
-const sliders = document.querySelectorAll('.slider__container');
-
-// On website load show only first slider
+const slides = document.querySelectorAll('.slider__container');
+let activeSlideId = 0;
+ 
+// On DOM load show only first slider - 
+// by default it's 0 but by changing activeSliderId it may start with different slide;
 document.addEventListener('DOMContentLoaded', () => {
-    sliders[1].style.display = 'none';
-})
-
-// add id to each slider container 
-sliders.forEach((slider, i) => slider.dataset.slideId = i)
-
-// on previous button click hide current slide and show previous
-prevButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        const slideId = +e.path[4].dataset.slideId;
-        if (slideId <= 0) return;
-        else {
-            sliders[slideId].style.display = 'none';
-            sliders[slideId-1].style.display = 'flex';
-        }
+    slides.forEach((slide, i) => {
+        // add id number to each slider container
+        slide.dataset.slideId = i;
+        // hide all slides beside the one with id set in activeSliderId
+        i !== activeSlideId ? slides[i].style.display = 'none' : null;
     })
+})
+prevButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => switchToPreviousSlide(+e.path[4].dataset.slideId));
+});
+            
+nextButtons.forEach(btn => {
+    btn.addEventListener('click', (e) => switchToNextSlide(+e.path[4].dataset.slideId));
 });
 
-// on next button click hide current slide and show next
-nextButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-        const slideId = +e.path[4].dataset.slideId;
-        if (slideId >= sliders.length - 1) return;
-        else {
-            sliders[slideId].style.display = 'none';
-            sliders[slideId+1].style.display = 'flex';
-        }
-    })
-})
+function switchToPreviousSlide(slideId) {
+    if (activeSlideId > 0) {
+        slides[activeSlideId].style.display = 'none';
+        slides[slideId - 1].style.display = 'flex';
+        activeSlideId--;
+    }
+    else {
+        slides[0].style.display = 'none';
+        slides[slides.length - 1].style.display = 'flex';
+        activeSlideId = slides.length - 1;
+    }
+}
 
-
+function switchToNextSlide(slideId) {
+    if (activeSlideId < slides.length - 1) {
+        slides[activeSlideId].style.display = 'none';
+        slides[slideId + 1].style.display = 'flex';
+        activeSlideId++;
+    }
+    else {
+        slides[activeSlideId].style.display = 'none';
+        slides[0].style.display = 'flex';
+        activeSlideId = 0;
+    }
+}
