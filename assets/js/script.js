cb2a3f0b632a2af00f5bde5ca69d58e52bd3aec7
apyd <arturpydych@gmail.com>
2020-10-10 22:14:33 +0200

    Animation on slide change

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ const prevButtons = document.querySelectorAll('.btn--left');
 const nextButtons = document.querySelectorAll('.btn--right');
 const slides = document.querySelectorAll('.slide__wrapper');
 let activeSlideId = 0;
+slides[activeSlideId].classList.add('loading');
  
 // On DOM load show only first slider - 
 // by default it's 0 but by changing activeSliderId it may start with different slide;
@@ -23,12 +24,16 @@ nextButtons.forEach(btn => {
 function switchToPreviousSlide() {
     if (activeSlideId > 0) {
         slides[activeSlideId].style.display = 'none';
+        slides[activeSlideId].classList.remove('loading');
         slides[activeSlideId - 1].style.display = 'flex';
+        slides[activeSlideId - 1].classList.add('loading');
         activeSlideId--;
     }
     else {
         slides[0].style.display = 'none';
+        slides[0].classList.remove('loading');
         slides[slides.length - 1].style.display = 'flex';
+        slides[slides.length - 1].classList.add('loading');
         activeSlideId = slides.length - 1;
     }
 }
@@ -36,12 +41,16 @@ function switchToPreviousSlide() {
 function switchToNextSlide() {
     if (activeSlideId < slides.length - 1) {
         slides[activeSlideId].style.display = 'none';
+        slides[activeSlideId].classList.remove('loading');
         slides[activeSlideId + 1].style.display = 'flex';
+        slides[activeSlideId + 1].classList.add('loading');
         activeSlideId++;
     }
     else {
         slides[activeSlideId].style.display = 'none';
+        slides[activeSlideId].classList.remove('loading');
         slides[0].style.display = 'flex';
+        slides[0].classList.add('loading');
         activeSlideId = 0;
     }
 }
